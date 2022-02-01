ffa4e5fc4a90a3ed83e59d6f2147d6ec164ec71f
ef-mojotrek <ef.mojotrek@gmail.com>
2022-02-01 15:19:28 -0700

    Added locations list

--- services/user/schema.js
+++ services/user/schema.js
@@ -5,7 +5,13 @@ const typeDefs = readFileSync(__dirname, 'schema.graphql');
 const users = [
     { id: '1', name: 'Ada Lovelace', email: 'ada_lovelace@email.com', locationId: "1" },
     { id: '2', name: 'Alan Turing', email: 'alan_turing@email.com', locationId: "2" },
+    { id: '3', name: 'Someone Else', email: 'someone_else@email.com', locationId: "1" },
   ];
+
+const locations = [
+  { id: '1', country: 'USA' },
+  { id: '2', country: 'notUSA' }
+]
 
 module.exports = makeExecutableSchema({
   typeDefs,
@@ -13,9 +19,6 @@ module.exports = makeExecutableSchema({
     User: {
       location: (user) => ({ id: user.locationId })
     },
-    // Location: {
-    //   users: (location) => users.find((user) => user.city === location)   
-    // },
     Query: {
         _location: (root, { id }) => ({ id, users: users.filter(user => user.locationId === id) }),
         user: (root, { id }) => users.find(user => user.id === id)
